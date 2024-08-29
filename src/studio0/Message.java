bfff94c2038f8cc71ec4793eb427c5b0bdc000b3
wang <wang@localhost>
2024-08-29 10:51:27 -0500

    Message to Aaron

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -4,6 +4,6 @@ public class Message {
 
 	public static void main(String args[]) {
 		System.out.println("Hi Alvin!");
-		System.out.println("Hi Aaron, stop fooling around.");
+		System.out.println("Hi Aaron, stop fooling around!");
 	}
 }
